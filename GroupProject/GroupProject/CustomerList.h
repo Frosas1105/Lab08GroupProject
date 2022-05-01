dbe170e865ce2b9f67c0b4f91865cf804cf23fd5
DavidBayekula <98560587+DavidBayekula@users.noreply.github.com>
2022-05-01 14:11:21 -0400

    Uploaded the customerList files

--- GroupProject/GroupProject/CustomerList.h
+++ GroupProject/GroupProject/CustomerList.h
@@ -1,14 +1,23 @@
-#pragma once
+#ifndef CUSTOMERLIST_H_INCLUDED
+#define CUSTOMERLIST_H_INCLUDED
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "linkedList.h"
 #include "Customer.h"
+using namespace std;
+
 
 class CustomerList :public linkedListType<Customer>
 {
 	friend ostream& operator<<(ostream&, const CustomerList&);
 public:
-	void AddCustomer(Customer&);
+	void AddCustomer(Customer& customer);
 	bool SearchCustomerByName(string) const;
-	Customer getCustomerByName(string) const;
-	void UpdateCustomer(Customer&);
-	void UpdateDataFile(ofstream&);
+	// void function to help Search Customer Name
+	void voidForSearchName(string searchByName, bool found, nodeType<Customer>* &current) const;
+	Customer getCustomerByName(string fname) const;
+	void UpdateCustomer(Customer& current);
+	void UpdateDataFile(ofstream& file);
 };
-
+#endif
